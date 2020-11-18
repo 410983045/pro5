bbc1f798a0b20e99eee98743909d3c55f1181dfd
410983045 <71751207+410983045@users.noreply.github.com>
2020-11-18 15:24:24 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    basic.clearScreen()
+})
